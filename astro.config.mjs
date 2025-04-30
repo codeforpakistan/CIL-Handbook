a37ec7c5a240326ddd7339f1474ff5200afa3093
Eman Amajd <you@example.com>
2025-04-30 09:30:02 +0500

    feat: remove 'Example Guide' from navigation and delete associated markdown file

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,14 +11,7 @@ export default defineConfig({
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-					
-				},
+			
 				{
 					label: 'Getting Started',
 					items: [
